c6413b16ea573bafb8dfca20ccf20b2191920331
Mohamad Ilyas Ali <108250794+yaszzzz@users.noreply.github.com>
2025-12-09 00:04:15 +0700

    update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="fixed top-0 w-full bg-[#31694E] shadow-md z-50">
+    <nav className="fixed top-0 w-full bg-[#658C58] shadow-md z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
